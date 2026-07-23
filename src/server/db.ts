90a2344f05f3c2317c9bd6345ccba3aa881a1c7b
Mohit Dayma <mohitdayama1234@gmail.com>
2026-07-23 22:13:17 +0530

    chore: Remove generated Prisma client files and update .gitignore to exclude generated directories

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,7 @@
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 import { env } from "@/env";
-import { PrismaClient } from "../../generated/prisma";
+import { PrismaClient } from "../generated/prisma";
 
 const pool = new pg.Pool({
 	connectionString: env.DATABASE_URL,
